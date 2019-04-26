a1be93fedd847024d212418394ebf6c18caa54be
Piotr Justyna <piotr.justyna@gmail.com>
2019-04-26 16:44:47 +0100

    Removed the target.

--- 1.3/hello_cargo/target/debug/deps/hello_cargo-178fe00226401c0c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\piotr_justyna\Documents\github\the-rust-programming-language\1.3\hello_cargo\target\debug\deps\hello_cargo-178fe00226401c0c.rmeta: src\main.rs
-
-C:\Users\piotr_justyna\Documents\github\the-rust-programming-language\1.3\hello_cargo\target\debug\deps\hello_cargo-178fe00226401c0c.d: src\main.rs
-
-src\main.rs:
--- 1.3/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\piotr_justyna\Documents\github\the-rust-programming-language\1.3\hello_cargo\target\debug\hello_cargo.pdb: C:\Users\piotr_justyna\Documents\github\the-rust-programming-language\1.3\hello_cargo\src\main.rs
